1b5c516e2f7859c18d8359668902fca6b6cf3f32
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 18:52:09 +0430

    adding jni lib to monitor threads

---
+++ app/src/main/cpp/native-thread.cpp
@@ -0,0 +1,29 @@
+//
+// Created by vincent on 5/8/21.
+//
+
+#include<cstring>
+#include<jni.h>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_ir_mrahimy_myndkapplication_MainNativeCallActivity_ndkThread(JNIEnv *env, jclass clazz,
+                                                                  jobject activity) {
+
+    if (JNI_OK != env->MonitorEnter(activity)) {
+        return env->NewStringUTF("Error in MonitorEnter");
+    }
+
+    jclass cls = env->GetObjectClass(activity);
+    jmethodID mid = env->GetMethodID(cls, "getClass", "()Ljava/lang/Class;");
+    jobject clsObj = env->CallObjectMethod(activity, mid);
+    cls = env->GetObjectClass(clsObj);
+    mid = env->GetMethodID(cls, "getName", "()Ljava/lang/String;");
+    auto strObj = (jstring) env->CallObjectMethod(clsObj, mid);
+
+    if (JNI_OK != env->MonitorExit(activity)) {
+        return env->NewStringUTF("Error in MonitorExit");
+    }
+
+    return strObj;
+}
