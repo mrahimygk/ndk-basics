17bede47c42560dace51e8d70995fdafe0de051e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 15:32:02 +0430

    adding a sample class with a method that returns string

---
+++ app/src/main/java/ir/mrahimy/myndkapplication/PhoneNumber.java
@@ -0,0 +1,14 @@
+package ir.mrahimy.myndkapplication;
+
+class PhoneNumber {
+    private final String phone;
+
+    PhoneNumber(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+}
