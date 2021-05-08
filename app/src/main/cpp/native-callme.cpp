45aeba67bd23ac1ceee829941a79cb116cb4a770
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 15:31:35 +0430

    adding new cpp method to call java class

---
+++ app/src/main/cpp/native-callme.cpp
@@ -0,0 +1,38 @@
+//
+// Created by vincent on 5/8/21.
+//
+
+#include<cstring>
+#include<jni.h>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_ir_mrahimy_myndkapplication_MainStringActivity_ndkCallMe(JNIEnv *env, jclass clazz,
+                                                              jobject phone_number) {
+
+    jstring pJstring;
+    jclass phoneNumberClass = env->GetObjectClass(phone_number);
+    jmethodID getPhone = env->GetMethodID(
+            phoneNumberClass,
+            "getPhone",
+            "()Ljava/lang/String;");
+
+    pJstring = (jstring) (env->CallObjectMethod(phone_number, getPhone));
+    const char *strReturn = env->GetStringUTFChars(pJstring, 0);
+
+
+    env->ReleaseStringUTFChars(pJstring, strReturn);
+
+
+    const char *str1 = "OK I will call this phone number ";
+
+    char result[1000];
+
+    strcpy(result, str1);
+    strcat(result, "\n");
+    strcat(result, strReturn);
+
+    return env->NewStringUTF(result);
+
+
+}
