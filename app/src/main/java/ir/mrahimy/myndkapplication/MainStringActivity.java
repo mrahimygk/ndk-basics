d718b61e6db161c4f930c9111798986a22558652
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 17:06:33 +0430

    using jni to catch java exceptions

--- app/src/main/java/ir/mrahimy/myndkapplication/MainStringActivity.java
+++ app/src/main/java/ir/mrahimy/myndkapplication/MainStringActivity.java
@@ -11,11 +11,14 @@ public class MainStringActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-concat");
         System.loadLibrary("native-callme");
+        System.loadLibrary("native-exception");
     }
 
     public static native String ndkGetHello(Activity mainStringActivity);
 
     public static native String ndkCallMe(PhoneNumber phoneNumber);
+
+    public static native String ndkException(ExceptionistClass exceptionistClass);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,10 @@ public class MainStringActivity extends AppCompatActivity {
 
         ((TextView) findViewById(R.id.textview)).setText(ndkGetHello(this));
         ((TextView) findViewById(R.id.phonetextview)).setText(ndkCallMe(new PhoneNumber("555-666-777")));
+
+        ((TextView) findViewById(R.id.exceptiontextview)).setText(
+                ndkException(new ExceptionistClass())
+        );
 
     }
 }
