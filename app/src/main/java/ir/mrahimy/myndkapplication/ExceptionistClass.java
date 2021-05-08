45382e651107e9523728a3c597e0bde8dfd7bc0c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 17:05:42 +0430

    adding a simple class that throws exceptions

---
+++ app/src/main/java/ir/mrahimy/myndkapplication/ExceptionistClass.java
@@ -0,0 +1,8 @@
+package ir.mrahimy.myndkapplication;
+
+class ExceptionistClass {
+
+    public void doTimeTravel() throws RuntimeException {
+        throw new RuntimeException("It is not possible to do time travel here");
+    }
+}
