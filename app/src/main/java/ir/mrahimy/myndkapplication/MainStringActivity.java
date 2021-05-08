e8e67dd1f32593a46ad97f539b971292c45729f1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-08 15:32:36 +0430

    using callme cpp lib to call a method from java

--- app/src/main/java/ir/mrahimy/myndkapplication/MainStringActivity.java
+++ app/src/main/java/ir/mrahimy/myndkapplication/MainStringActivity.java
@@ -10,9 +10,12 @@ public class MainStringActivity extends AppCompatActivity {
 
     static {
         System.loadLibrary("native-concat");
+        System.loadLibrary("native-callme");
     }
 
     public static native String ndkGetHello(Activity mainStringActivity);
+
+    public static native String ndkCallMe(PhoneNumber phoneNumber);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,7 @@ public class MainStringActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_string);
 
         ((TextView) findViewById(R.id.textview)).setText(ndkGetHello(this));
+        ((TextView) findViewById(R.id.phonetextview)).setText(ndkCallMe(new PhoneNumber("555-666-777")));
+
     }
 }
